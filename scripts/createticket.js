1d25b6ae46d52d3b953d118ef06c0c81339c1231
JTu8 <htmjaet8@gmail.com>
2019-05-03 15:19:40 +0300

    Create ja update tehty

--- scripts/createticket.js
+++ scripts/createticket.js
@@ -3,5 +3,28 @@
 var api = require("servicenow-lite");
 
 module.exports = function(robot) {
+    robot.commands.push(
+        "Enfobot create <prefix> with <Short description> - Creates a new ticket"
+    );
+    // Creates a new ticket
+    robot.respond(/create (.*) with (.*)/i, function(response) {
+        var prefix = response.match[1];
+        var shortDescription = response.match[2];
 
+        var params = {
+            'short_description': shortDescription
+        }
+
+        api.createTicket(params, prefix, function(err, result) {
+            if (err) {
+                response.send("Ticket creation failed");
+                console.error(err);
+                return;
+            }
+            else {
+                response.send("New ticket created");
+                response.send(JSON.stringify(result));
+            }
+        });
+    });
 };
---
+++ scripts/updateticket.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var api = require("servicenow-lite");
+
+module.exports = function(robot) {
+    robot.commands.push(
+        "Enfobot update ticket <number> from <prefix> with <Short description> - Updates selected tickets short description field"
+    );
+
+    robot.respond(/update ticket (.*) from (.*) with (.*)/i, function (response) {
+        var ticketNumber = response.match[1];
+        var prefix = response.match[2];
+        var param = response.match[3];
+        var updateParams;
+
+        console.log("Ticket number = " + ticketNumber);
+        console.log("Prefix = " + prefix);
+        console.log("Param = " + param);
+        // Gets sys_id 
+        api.getRecordById(ticketNumber, function(err, result) {
+            if (err) {
+                response.send("Ticket number was not found, please try again");
+                console.error(err);
+            }
+            else {
+                updateParams = {
+                    'sys_id': result.sys_id,
+                    'short_description': param
+                };
+                console.log("Params = " + JSON.stringify(updateParams));
+
+                // Updates ticket
+                api.updateTicket(updateParams, prefix, function(err, result) {
+                    if (err) {
+                        response.send("Update failed, please try again");
+                        console.error(err);
+                        return;
+                    }
+                    else {
+                        response.send("Ticket updated");
+                        response.send(JSON.stringify(result));
+                    }
+            });
+            }
+        });
+        
+
+    });
+};
