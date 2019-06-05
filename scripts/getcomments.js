0b5bcd0427f1836f6e1e9e7deaa0b2e1d461b8ed
Jere Tuppurainen <Jere.Tuppurainen@enfogroup.com>
2019-06-05 15:02:11 +0300

    Kommenttien hakemista jatkettu

--- scripts/getcomments.js
+++ scripts/getcomments.js
@@ -9,7 +9,36 @@ module.exports = function(robot) {
 
     robot.respond(/get comments from (.*)/i, function(response) {
         var taskNumber = response.match[1];
+        var taskParams;
+        var comments;
+        
+        api.getRecordById(taskNumber, function(err, result) {
+            if (err) {
+                response.send("Task was not found, please try again");
+                console.error(err);
+                return;
+            }
+            else {
+                taskParams = {
+                    'sys_id': result.sys_id
+                };
+                console.log(JSON.stringify(taskParams));
+            }
+
+            api.getCommentByTask(taskParams, function(err, result) {
+                if (err) {
+                    response.send("Comment fetching failed, please try again");
+                    console.error(err);
+                    return;
+                }
+                else {
+                    response.send("Succeess!");
+                    console.log(JSON.stringify(result['comments']));
+                }
+            });
+        });
+
+    });
 
         
-    });
 };
