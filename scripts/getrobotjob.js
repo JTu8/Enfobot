2a781f043d28f329f0650b9dd0ded85f6b2d8c3e
JTu8 <htmjset8@gmail.com>
2020-08-14 11:46:26 +0300

    Päivityksiä API kutsuihin

---
+++ scripts/getrobotjob.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const { weekdaysMin } = require("moment");
+
+module.exports = function(robot) {
+    robot.commands.push(
+        "Enfobot get job <job name> status - Prints status of a selected job"
+    );
+
+    robot.respond(/get job (.*) status/i, function(response) {
+        var job = response.match[1];
+
+        const authenticateUrl = process.env.UIPATH_ORCH_URL + "api/account/authenticate";
+        const robotsUrl = process.env.UIPATH_ORCH_URL + "odata/Robots";
+        const releasesUrl = process.env.UIPATH_ORCH_URL + "odata/Releases";
+        const getJobUrl = process.env.UIPATH_ORCH_URL + "odata/Jobs/?&$filter=";
+
+        var tenant = process.env.TENANT_NAME;
+        console.log(tenant);
+
+        var username = process.env.UIPATH_USERNAME;
+        console.log(username);
+
+        var password = process.env.UIPATH_PASSWORD;
+        console.log(password);
+
+        var data;
+        data = JSON.stringify(
+            {
+                "tenancyName": tenant,
+	            "usernameOrEmailAddress": username,
+	            "password": password
+            }
+        );
+        console.log(data);
+
+        robot.http(authenticateUrl).header('Content-Type', 'application/json').post(data)(function(err, res, body) {
+            if(res.statusCode !== 200) {
+                response.send("Error while authenticating");
+                console.log("Error while authenticating: " + err);
+                return;
+            }
+            else {
+                var token = JSON.parse(body);
+                console.log('Bearer ' + token.result);
+
+                var auth = 'Bearer ' + token.result;
+
+                robot.http(releasesUrl + '?$filter=ProcessKey eq ' + "'" + job + "'").header('Authorization', auth).get()(function(err, res, body) {
+                    if(res.statusCode !== 200) {
+                        response.send("Error while getting Release name");
+                        console.log("Error while getting Release name " + err);
+                        return;
+                    }
+                    else {
+                        var release = JSON.parse(body);
+                        console.log(release);
+                        console.log("Job Process key: " + release.value[0].ProcessKey);
+                        console.log("Job Environment name: " + release.value[0].EnvironmentName);
+                        var releaseKey = release.value[0].ProcessKey;
+                        var environmentName = release.value[0].EnvironmentName;
+                        var releaseName = releaseKey + "_" + environmentName;
+                        console.log("Job release name: " + releaseName);
+
+                        robot.http(getJobUrl + 'ReleaseName eq ' + "'" + releaseName + "'").header('Authorization', auth).get()(function(err, res, body) {
+                            if(res.statusCode !== 200) {
+                                response.send("Error while getting Job data");
+                                console.log("Error while getting Job data ",  + err);
+                                return;
+                            }
+                            else {
+                                var jobStatus = JSON.parse(body);
+
+                                for (var i in jobStatus.value) {
+                                    var startTime = jobStatus.value[i].StartTime;
+                                    var endTime = jobStatus.value[i].EndTime;
+                                    var state = jobStatus.value[i].State;
+                                    var info = jobStatus.value[i].Info;
+                                    var inputArguments = jobStatus.value[i].InputArguments;
+                                    var outPutArguments = jobStatus.value[i].OutputArguments;
+                                }
+
+                                response.send("Status for job " + job + "\n" + 
+                                    "Start time: " + startTime + "\n" + 
+                                    "End time: " + endTime + "\n" + 
+                                    "State: " + state + "\n" + 
+                                    "Info: " + info + "\n" + 
+                                    "Input arguments: " + inputArguments + "\n" + 
+                                    "Output arguments: " + outPutArguments);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+};
--- scripts/listprocesses.js
+++ scripts/listprocesses.js
@@ -1,17 +1,14 @@
 "use strict";
 
-
 module.exports = function(robot) {
     robot.commands.push(
-        "Enfobot get all robots - Prints info about all robots"
+        "Enfobot get all processes - Prints all processes"
     );
 
-    
-    robot.respond(/get all robots/i, function(response) {
+    robot.respond(/get all processes/i, function(response) {
 
-        
         const authenticateUrl = process.env.UIPATH_ORCH_URL + "api/account/authenticate";
-        const robotsUrl = process.env.UIPATH_ORCH_URL + "odata/Robots";
+        const releasesUrl = process.env.UIPATH_ORCH_URL + "odata/Releases";
 
         var tenant = process.env.TENANT_NAME;
         console.log(tenant);
@@ -21,8 +18,6 @@ module.exports = function(robot) {
 
         var password = process.env.UIPATH_PASSWORD;
         console.log(password);
-
-
 
         var data;
         data = JSON.stringify(
@@ -47,33 +42,28 @@ module.exports = function(robot) {
 
                 var auth = 'Bearer ' + token.result;
 
-                robot.http(robotsUrl).header('Authorization', auth).get()(function(err, res, body) {
+                robot.http(releasesUrl).header('Authorization', auth).get()(function(err, res, body) {
                     if(res.statusCode !== 200) {
                         response.send("Error: " + err);
                         console.log("Error:" + err);
                         return;
                     }
                     else {
-                        var robots = [];
-                        robots = JSON.parse(body);
-                        console.log(robots);
-                        
-                        robots.value.map(item => {
-                            console.log("Robots: " + item.Name);
-                            console.log("ID:"  + item.Id);
-                            console.log("Machine host name: " + item.MachineName);
-                            response.send("Robots name: " + item.Name + "\n" + "Robots Id: " + item.Id + "\n" + "Machine hostname: " + item.MachineName);
+                        var processes = [];
+                        processes = JSON.parse(body);
+                        console.log(processes);
+
+                         processes.value.map(item => {
+                            console.log("Prosessit: " + item.ProcessKey);
+                            response.send(item.ProcessKey);
                         });
-
-
+                       
+                    
                         
                     }
                 });
             }
         });
 
-        
-
     });
-
 };
